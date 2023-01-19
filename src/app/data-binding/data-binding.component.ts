1ff72d7ea21b643ec9918d6be45b0029cbc34945
Isadora <rm84246@fiap.com.br>
2023-01-18 21:56:22 -0300

    Property Binding e Interpolação

--- src/app/data-binding/data-binding.component.ts
+++ src/app/data-binding/data-binding.component.ts
@@ -7,4 +7,17 @@ import { Component } from '@angular/core';
 })
 export class DataBindingComponent {
 
+  public url: string = 'uuuuuhuuuu.com';
+  public urlDmC: string = 'https://staticdelivery.nexusmods.com/mods/2751/images/798/798-1600863312-1511229545.jpeg';
+
+  public aprendeu: boolean = true;
+
+  public get valor(): number{
+    return 60 * 9;
+  }
+
+  public get aprendizado(): boolean{
+    return true;
+  }
+
 }
